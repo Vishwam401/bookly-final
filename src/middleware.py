1622b07dd11dd638fb28099beb5a14f80142795c
Vishwam401 <vishwamvaghasiya401@gmail.com>
2026-04-16 03:41:13 +0530

    add prof URL to trusted hosts

--- src/middleware.py
+++ src/middleware.py
@@ -36,7 +36,7 @@ def register_middleware(app: FastAPI):
 
     app.add_middleware(
         TrustedHostMiddleware,
-        allowed_hosts= ['localhost', "127.0.0.1", 'testserver'],
+        allowed_hosts= ['localhost', "127.0.0.1", "bookly-api-1ncq.onrender.com/"],
     )
 
 
