8a1442990d0b95911c13cfb5ba25d4192bdd18ae
Vishwam401 <vishwamvaghasiya401@gmail.com>
2026-04-16 03:20:35 +0530

    fix:config

--- src/config.py
+++ src/config.py
@@ -1,25 +1,31 @@
+import os
+from pathlib import Path
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+# Base directory setup (src folder)
+BASE_DIR = Path(__file__).resolve().parent
 
 class Settings(BaseSettings):
     DATABASE_URL: str
     JWT_SECRET: str
     JWT_ALGORITHM: str
-    REDIS_URL: str = "redis://localhost:6379/0"
+    REDIS_URL: str = "redis://localhost:6379/0" # Render env mein override ho jayega
     MAIL_USERNAME: str
-    MAIL_PASSWORD : str
-    MAIL_FROM : str
-    MAIL_PORT :int
-    MAIL_SERVER :str
+    MAIL_PASSWORD: str
+    MAIL_FROM: str
+    MAIL_PORT: int
+    MAIL_SERVER: str
     MAIL_FROM_NAME: str
-    MAIL_STARTTLS:bool = True
-    MAIL_SSL_TLS:bool = False
+    MAIL_STARTTLS: bool = True
+    MAIL_SSL_TLS: bool = False
     USE_CREDENTIALS: bool = True
-    VALIDATE_CERTS:bool  = True
+    VALIDATE_CERTS: bool = True
     DOMAIN: str = "localhost:8000"
 
-    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    # Template path setup jo Linux/Render pe na phate
+    TEMPLATE_FOLDER: str = str(BASE_DIR / "templates")
 
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 Config = Settings()
 
