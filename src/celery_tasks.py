73142fddbb47541143587f81efb580987b49e2f3
Vishwam401 <vishwamvaghasiya401@gmail.com>
2026-04-16 04:59:13 +0530

    fix celery_tasks

--- src/celery_tasks.py
+++ src/celery_tasks.py
@@ -1,10 +1,21 @@
 from celery import Celery
 from src.mail import mail, create_message
 from asgiref.sync import async_to_sync
+from src.config import Config
 
-c_app = Celery()
+c_app = Celery("tasks") # Task name de do
 
 c_app.config_from_object('src.config')
+
+# Render Redis SSL Fix: Ye settings compulsory hain
+c_app.conf.update(
+    broker_use_ssl={
+        'ssl_cert_reqs': 'none'
+    },
+    redis_backend_use_ssl={
+        'ssl_cert_reqs': 'none'
+    }
+)
 
 @c_app.task()
 def send_email(recipients: list[str], subject: str , body : str):
@@ -14,5 +25,6 @@ def send_email(recipients: list[str], subject: str , body : str):
         body=body,
     )
 
+    # FastAPI-Mail async hai, isliye async_to_sync sahi hai
     async_to_sync(mail.send_message)(message)
-    print("Email sent")
+    print("Email sent successfully!")
